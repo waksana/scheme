2383d5a552abf187f55ba4dea55fdfe3ca093a77
whl <wanghonglai@innobuddy.com>
2017-06-28 14:37:52 +0800

    change style

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-
 const scheme = require('./scheme.js');
-
-var cmd=document.getElementById("cmd");
-var bodyel=document.body;
-var focusinput = () => cmd.focus();
-var res=document.getElementById("out");
-
-bodyel.onclick=focusinput;
-bodyel.onfocus=focusinput;
-
-focusinput();
 
 const ev = str => {
   var res;
@@ -21,12 +10,15 @@ const ev = str => {
   }
   return res;
 }
-window.document.onkeydown=function(event){
-  const ec=event.keyCode;
-  if(ec===13){
-    res.innerHTML+="> "+cmd.value+"<br />";
-    res.innerHTML+=ev(cmd.value)+"<br />";
-    cmd.value="";
-    bodyel.scrollTop=bodyel.scrollHeight;
-  }
+
+var cmd = document.getElementById("code");
+var res = document.getElementById("res");
+
+function refresh() {
+  var value = ev(cmd.value);
+  if(value instanceof Array) value = value.join('\n');
+  res.value = value;
 }
+
+cmd.onkeyup = cmd.onchange = refresh;
+
