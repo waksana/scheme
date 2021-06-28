b39b6442039d4a604e237e4ea3d0ef71ef6cc60f
honglai <waksana@qq.com>
2021-06-28 19:12:24 +0800

    add haskell parser for vml

--- utils/vmParser.hs
+++ utils/vmParser.hs
@@ -8,6 +8,7 @@ data Expression = Number Integer
                 | Add Expression Expression
                 | Mul Expression Expression
                 | Div Expression Expression
+                | Mod Expression Expression
 
 data Instruction = Label String
                  | Assign String Expression
@@ -16,4 +17,21 @@ data Instruction = Label String
                  | Restore String
                  | Goto String
 
-label = many1 letter <$> Label
+label :: Parsec String () Instruction
+label = Label <$> many1 letter 
+
+operator :: Parsec String () Char
+operator = oneOf "+-*/%="
+
+numExpression:: Parsec String () Expression
+numExpression = Number . read <$> many1 digit
+
+matchWord :: String -> Parsec String () String
+matchWord [] = return []
+matchWord (x:xs) = do
+    rx <- char x
+    rxs <- matchWord xs
+    return (rx:rxs)
+
+parenthesis:: Parsec String () a -> Parsec String () a
+parenthesis p = char '(' *> p <* char ')'
